5672a0b8937ce09fd9aeab8cc055b6d009c2783c
Amish Bisaria <amish.bisaria@gmail.com>
2021-03-20 01:23:50 -0500

    Updated GUI and added date section implemented default date on program start

--- src/Data/ScheduleItem.java
+++ src/Data/ScheduleItem.java
@@ -5,12 +5,12 @@ import java.time.LocalDate;
 public class ScheduleItem {
 
     private String info;
-    private String allInfo;
+    private String fullInfo;
     private LocalDate dueDate;
 
-    public ScheduleItem(String info, String allInfo, LocalDate dueDate) {
+    public ScheduleItem(String info, String fullInfo, LocalDate dueDate) {
         this.info = info;
-        this.allInfo = allInfo;
+        this.fullInfo = fullInfo;
         this.dueDate = dueDate;
     }
 
@@ -22,12 +22,12 @@ public class ScheduleItem {
         this.info = info;
     }
 
-    public String getAllInfo() {
-        return allInfo;
+    public String getFullInfo() {
+        return fullInfo;
     }
 
-    public void setAllInfo(String allInfo) {
-        this.allInfo = allInfo;
+    public void setFullInfo(String fullInfo) {
+        this.fullInfo = fullInfo;
     }
 
     public LocalDate getDueDate() {
@@ -36,5 +36,10 @@ public class ScheduleItem {
 
     public void setDueDate(LocalDate dueDate) {
         this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return info;
     }
 }
--- src/Schedule/Controller.java
+++ src/Schedule/Controller.java
@@ -1,15 +1,29 @@
 package Schedule;
 
 import Data.ScheduleItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
 
     private List<ScheduleItem> schItems;
+
+    @FXML
+    private ListView<ScheduleItem> scheduleView;
+
+    @FXML
+    private TextArea fullInfoTextArea;
+
+    @FXML
+    private Label dueLabel;
 
     public void initialize()
     {
@@ -26,8 +40,31 @@ public class Controller {
         schItems.add(item4);
         schItems.add(item5);
 
+        //when the selection is changed update text for text area
+        scheduleView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<ScheduleItem>() {
+            @Override
+            public void changed(ObservableValue<? extends ScheduleItem> observableValue, ScheduleItem scheduleItem, ScheduleItem t1) {
+                if (t1 != null)
+                {
+                    ScheduleItem item = scheduleView.getSelectionModel().getSelectedItem();
+                    fullInfoTextArea.setText(item.getFullInfo());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    dueLabel.setText(df.format(item.getDueDate()));
+                }
+            }
+        });
+
+        scheduleView.getItems().setAll(schItems); //puts items in schItems into scheduleView
+        scheduleView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE); //lets only one item be selected
+        scheduleView.getSelectionModel().selectFirst();
     }
 
-
+    @FXML
+    public void clickedListView()
+    {
+        ScheduleItem item = scheduleView.getSelectionModel().getSelectedItem(); //gets the selected item
+        fullInfoTextArea.setText(item.getFullInfo());
+        dueLabel.setText(item.getDueDate().toString());
+    }
 
 }
